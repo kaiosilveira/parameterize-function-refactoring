36e8dc1cace490d3e64cd743169aaef54c53e406
Kaio Silveira <silveira.kaio@icloud.com>
2024-09-25 18:45:58 +0100

    usage-charge: replace call to `topBand` with `withinBand` at `baseCharge`

--- src/usage-charge/index.js
+++ src/usage-charge/index.js
@@ -8,8 +8,12 @@ function usd(amount) {
 
 export function baseCharge(usage) {
   if (usage < 0) return usd(0);
+
   const amount =
-    withinBand(usage, 0, 100) * 0.03 + withinBand(usage, 100, 200) * 0.05 + topBand(usage) * 0.07;
+    withinBand(usage, 0, 100) * 0.03 +
+    withinBand(usage, 100, 200) * 0.05 +
+    withinBand(usage, 200, Infinity) * 0.07;
+
   return usd(amount);
 }
 
