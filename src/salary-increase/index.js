02e4d96ebf6b573ab1c9ae0e00328200da0e5bb8
Kaio Silveira <silveira.kaio@icloud.com>
2024-09-25 18:34:28 +0100

    salary-increase: call `raise` at `fivePercentRaise` and `tenPercentRaise`

--- src/salary-increase/index.js
+++ src/salary-increase/index.js
@@ -1,9 +1,9 @@
 export function tenPercentRaise(aPerson) {
-  aPerson.salary = aPerson.salary.multiply(1.1);
+  raise(aPerson, 0.1);
 }
 
 export function fivePercentRaise(aPerson) {
-  aPerson.salary = aPerson.salary.multiply(1.05);
+  raise(aPerson, 0.05);
 }
 
 export function raise(aPerson, factor) {
